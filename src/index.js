f518993699787b45c6766ad747de8eda658f5bc7
IzzyTrott <isabeltrottphotography@gmail.com>
2024-11-01 16:09:04 +0000

    added search functionality with API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-function getCityWeather(event) {
+function updateCityWeather(response) {
+  let temperatureElement = document.querySelector("#weather-temperature");
+  let cityElement = document.querySelector("#city");
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "475c1b09tc9cf784eac1636c9abbbco2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateCityWeather);
+}
+
+function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", getCityWeather);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
