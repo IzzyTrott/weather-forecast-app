5ab96682fc6763c9c57ea94f42696ec08f1bfb1f
IzzyTrott <isabeltrottphotography@gmail.com>
2024-11-01 17:58:38 +0000

    added js for more weather data using api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 function updateCityWeather(response) {
   let temperatureElement = document.querySelector("#weather-temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#weather-icon");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon"
+              />`
 }
 
 function searchCity(city) {
